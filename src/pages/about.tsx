4d72b79a0ffc0a0d19fb17fd15b48e2b9f7cc8e0
Forrest Lyman <161376305+forrest-lyman@users.noreply.github.com>
2025-01-24 07:22:32 -0800

    creates webview provider

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { provideWebview } from '../lib/webview/provider';
 
 const About: React.FC = () => {
     return (
@@ -9,5 +9,4 @@ const About: React.FC = () => {
     );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<About />);
+export default provideWebview(About);
